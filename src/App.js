dcf672b5ff6bddde8268297ce47bcc02daa322a1
houssem115 <houssem_sf2010@live.fr>
2021-05-23 11:39:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import{ Map, TileLayer, Marker } from 'react-leaflet';
+import geolocated from 'react'
+const DEFAULT_LANGITUDE =-123;
+const DEFAULT_LATITUDE =48;
 
-function App() {
+class App extends React.Component {
+  render(){
+  const langitude =this.props.coords ? this.props.coords.langitude:DEFAULT_LANGITUDE
+  const latitude =this.props.coords ? this.props.coords.latitude:DEFAULT_LATITUDE
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Map center={[DEFAULT_LATITUDE ,DEFAULT_LANGITUDE]} zoom={1}>
+ 
+ <tileLayer 
+ url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+ attribution='&copy; <a href="http://osm.org/copyright">openStret</a>contributors'
+ />
+{
+  !this.props.coords?
+  <div className="lowding">lowww</div>
+  :
+  <Marker
+  position={[langitude, latitude]}>
+    <Popup>YOU ARE HERE</Popup>
+  </Marker>
 }
 
-export default App;
+
+    </Map>
+
+  );
+}
+}
+export default geolocated({
+positionOptions:{
+  enableHighAccuracy:false
+}
+,
+userDecisionTimeout:10000
+})
+(App)
